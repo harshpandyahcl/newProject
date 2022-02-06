eeb1ddbfe4618e1401ffaa107061f8da1f75a86c
harshpandyahcl <harsh.pandya@hcl.com>
2022-02-06 16:12:48 -0600

    First commit

---
+++ src/main/java/com/hcl/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.hcl.repositories;
+
+import com.hcl.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+
+    User findByUsername(String username);
+    User getById(Long id);
+}
