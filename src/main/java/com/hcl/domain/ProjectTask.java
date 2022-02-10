822f2b5250fc5af5ae5f0d2523ffa5125b49896e
harshpandyahcl <harsh.pandya@hcl.com>
2022-02-09 21:54:16 -0600

    update new

--- src/main/java/com/hcl/domain/ProjectTask.java
+++ src/main/java/com/hcl/domain/ProjectTask.java
@@ -1,14 +1,12 @@
 package com.hcl.domain;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
-
 @Entity
 public class ProjectTask {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,6 +17,7 @@ public class ProjectTask {
     private String acceptanceCriteria;
     private String status;
     private Integer priority;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date dueDate;
     //ManyToOne with Backlog
     @ManyToOne(fetch = FetchType.EAGER)
@@ -28,110 +27,87 @@ public class ProjectTask {
 
     @Column(updatable = false)
     private String projectIdentifier;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date create_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date update_At;
 
     public ProjectTask() {
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getProjectSequence() {
         return projectSequence;
     }
-
     public void setProjectSequence(String projectSequence) {
         this.projectSequence = projectSequence;
     }
-
     public String getSummary() {
         return summary;
     }
-
     public void setSummary(String summary) {
         this.summary = summary;
     }
-
     public String getAcceptanceCriteria() {
         return acceptanceCriteria;
     }
-
     public void setAcceptanceCriteria(String acceptanceCriteria) {
         this.acceptanceCriteria = acceptanceCriteria;
     }
-
     public String getStatus() {
         return status;
     }
-
     public void setStatus(String status) {
         this.status = status;
     }
-
     public Integer getPriority() {
         return priority;
     }
-
     public void setPriority(Integer priority) {
         this.priority = priority;
     }
-
     public Date getDueDate() {
         return dueDate;
     }
-
     public void setDueDate(Date dueDate) {
         this.dueDate = dueDate;
     }
-
     public String getProjectIdentifier() {
         return projectIdentifier;
     }
-
     public void setProjectIdentifier(String projectIdentifier) {
         this.projectIdentifier = projectIdentifier;
     }
-
     public Date getCreate_At() {
         return create_At;
     }
-
     public void setCreate_At(Date create_At) {
         this.create_At = create_At;
     }
-
     public Date getUpdate_At() {
         return update_At;
     }
-
     public void setUpdate_At(Date update_At) {
         this.update_At = update_At;
     }
-
     public Backlog getBacklog() {
         return backlog;
     }
-
     public void setBacklog(Backlog backlog) {
         this.backlog = backlog;
     }
-
     @PrePersist
     protected void onCreate(){
         this.create_At = new Date();
     }
-
     @PreUpdate
     protected void onUpdate(){
         this.update_At = new Date();
     }
-
     @Override
     public String toString() {
         return "ProjectTask{" +
